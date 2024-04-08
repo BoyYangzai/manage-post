72c888db9110905de34896a5d4b2f4530bcf6ed1
洋 <94534613+BoyYangzai@users.noreply.github.com>
2024-04-08 11:48:38 +0800

    feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import * as React from 'react';
-import { PaletteMode } from '@mui/material';
+import { Avatar, Card, Container, Link, PaletteMode } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
@@ -8,17 +8,16 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
-import AppAppBar from '../components/AppAppBar';
-import Hero from '../components/Hero';
-import LogoCollection from '../components/LogoCollection';
-import Highlights from '../components/Highlights';
-import Pricing from '../components/Pricing';
-import Features from '../components/Features';
-import Testimonials from '../components/Testimonials';
-import FAQ from '../components/FAQ';
-import Footer from '../components/Footer';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
 import getLPTheme from './getLPTheme';
-
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Editor } from '@wangeditor/editor-for-react';
+import dynamic from 'next/dynamic';
+import DeleteIcon from '@mui/icons-material/Delete';
+const WangEditor = dynamic(
+  () => import('../components/Editor'),
+  {ssr: false}
+)
 interface ToggleCustomThemeProps {
   showCustomTheme: Boolean;
   toggleCustomTheme: () => void;
@@ -63,10 +62,12 @@ function ToggleCustomTheme({
 }
 
 export default function LandingPage() {
+  const [currentTab, setCurrentTab] = React.useState(1);
   const [mode, setMode] = React.useState<PaletteMode>('light');
   const [showCustomTheme, setShowCustomTheme] = React.useState(true);
   const LPtheme = createTheme(getLPTheme(mode));
   const defaultTheme = createTheme({ palette: { mode } });
+  const [posts,setPosts]=React.useState([]);
 
   const toggleColorMode = () => {
     setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
@@ -78,27 +79,195 @@ export default function LandingPage() {
 
   return (
     <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
-      <CssBaseline />
-      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
-      <Hero />
-      <Box sx={{ bgcolor: 'background.default' }}>
-        <LogoCollection />
-        <Features />
-        <Divider />
-        <Testimonials />
-        <Divider />
-        <Highlights />
-        <Divider />
-        <Pricing />
-        <Divider />
-        <FAQ />
-        <Divider />
-        <Footer />
-      </Box>
-      <ToggleCustomTheme
-        showCustomTheme={showCustomTheme}
-        toggleCustomTheme={toggleCustomTheme}
-      />
+      <Container style={{
+        width: '60%',
+        display: 'flex',
+        flexDirection:'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        flexWrap: 'wrap',
+        marginTop: '20px',
+      }}>
+        <div style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          gap: '10px',
+        }}>
+       <ArrowBackIcon></ArrowBackIcon> Back
+      </div>
+        <h2 style={{
+          width: '100%',
+          textAlign: 'center',
+        }}>Manage Post</h2>
+        <Card
+          sx={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            ":hover": {
+              cursor: 'pointer',
+            }
+          }}
+          onClick={() => {
+            // add new tab to https://www.github.com/boyyangzai
+            window.open('https://www.github.com/boyyangzai', '_blank');
+          }}
+        >
+          <Avatar
+            sx={{
+              width: '100px',
+              height: '100px',
+              mr:5
+            }}
+            variant="rounded" alt="yang" src="https://avatars.githubusercontent.com/u/94534613?v=4" />
+          <div>
+            <div style={{
+              color:'#606060'
+            }}>yang</div>
+            <a
+              style={{
+                color: '#1F1F2C',
+                textDecoration: 'none',
+              }}
+              href='https://www.github.com/boyyangzai' target='__blank' >https://www.github.com/boyyangzai</a>
+          </div>
+        </Card>
+        <Card
+          sx={{
+            marginTop: '20px',
+            width: '100%',
+            height: '45px',
+            display: 'flex',
+               ':hover': {
+                      cursor: 'pointer',
+                    },
+        }}>
+          {
+            ['Settings', 'Post'].map((i,index) => {
+              return (
+                <div
+                  onClick={() => {
+                    setCurrentTab(index);
+                  }
+                  }
+                  style={{
+                  flex: 1,
+                    backgroundColor: currentTab === index ? '#532BC5' : '#ffffff',
+                  color: currentTab === index ? '#ffffff' : '#000000',
+                  display: 'flex',
+                  justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                  {i}
+                  {
+                    index === 1 &&
+                    <span style={{
+                       marginLeft: '5px',
+                    width: '20px',
+                    height: '20px',
+                    borderRadius: '50%',
+                    background: currentTab === index ? '#ffffff' : '#000000',
+                        color: currentTab === index ? '#532BC5' : 'white',
+                        display: 'flex',
+
+                        justifyContent: 'center', 
+                        alignItems: 'center',
+                  }}>
+                {posts.length}
+                  </span>
+                 }
+                </div>
+              )
+            })
+          }
+          
+        </Card>
+        <WangEditor setPosts={setPosts}></WangEditor>
+
+        {
+          posts.map((post,index) => {
+            return (
+
+              <div
+                key={index}
+                style={{
+                  marginTop: '40px',
+                  width: '100%',
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent:'center',
+                  position: 'relative',
+                  overflow: 'none',
+                }}>
+                <div style={{
+                  width: '90%',
+                      display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  borderTop: '1px solid #ccc',
+                }}>
+                  <div style={{
+                    position: 'absolute',
+                    borderRadius: '30px',
+                    top: '-14px',
+                    left:'-4px',
+                    backgroundColor: '#532BC5',
+                    padding: '5px',
+                    paddingRight: '20px',
+                    paddingLeft: '20px',
+                    color: 'white',
+                    fontWeight: 'bold',
+                  }}> {post.title}</div>
+                       <div style={{
+                    position: 'absolute',
+                    borderRadius: '30px',
+                    top: '-14px',
+                    right:'20px',
+                    backgroundColor: '#532BC5',
+                    padding: '5px',
+                    paddingRight: '20px',
+                    paddingLeft: '20px',
+                    color: 'white',
+                  }}><BorderColorIcon style={{
+                    fontSize: '20px',
+                    }}></BorderColorIcon> Edit</div>
+                  <div
+                    onClick={() => [
+                      setPosts((posts) => {
+                        return posts.filter((_,i) => i !== index)
+                      })
+                    ]}
+                    style={{
+                    width: '35PX',
+                    height: '35PX',
+                    position: 'absolute',
+                    borderRadius: '50%',
+                    top: '-14px',
+                    right:'-14px',
+                    backgroundColor: '#C4C4C4',
+                    color: 'white',
+                    display: 'flex',
+                    justifyContent: 'center',
+                      alignItems: 'center',
+                  }}><DeleteIcon
+                      style={{
+                    fontSize: '20px',
+                    }}></DeleteIcon></div>
+                  <h3 style={{
+                    marginTop: '40px',
+                  }}>{post.title}</h3>
+                <div dangerouslySetInnerHTML={{__html: post.content}}></div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </Container>
     </ThemeProvider>
   );
 }
---
+++ src/components/Editor.tsx
@@ -0,0 +1,119 @@
+'use client'
+import '@wangeditor/editor/dist/css/style.css' // 引入 css
+
+import React, { useState, useEffect } from 'react'
+import { Editor, Toolbar } from '@wangeditor/editor-for-react'
+import { IDomEditor, IEditorConfig, IToolbarConfig } from '@wangeditor/editor'
+import { Alert, Button, Card, Input } from '@mui/material'
+import CheckIcon from '@mui/icons-material/Check';
+import { Message } from '@mui/icons-material'
+function MyEditor({ setPosts }) {
+  const[alert, setAlert] = useState(false)
+    // editor 实例
+    const [editor, setEditor] = useState<IDomEditor | null>(null)   // TS 语法
+    // const [editor, setEditor] = useState(null)                   // JS 语法
+
+  // 编辑器内容
+  const [title, setTitle] = useState('')
+    const [html, setHtml] = useState('')
+
+
+    // 工具栏配置
+    const toolbarConfig: Partial<IToolbarConfig> = { }  // TS 语法
+    // const toolbarConfig = { }                        // JS 语法
+
+    // 编辑器配置
+    const editorConfig: Partial<IEditorConfig> = {    // TS 语法
+    // const editorConfig = {                         // JS 语法
+        placeholder: '请输入内容...',
+    }
+
+    // 及时销毁 editor ，重要！
+    useEffect(() => {
+        return () => {
+            if (editor == null) return
+            editor.destroy()
+            setEditor(null)
+        }
+    }, [editor])
+
+  return (
+    <div style={{
+      marginTop: '20px',
+    }}>
+      {
+        alert && <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+          Post successfully!
+</Alert>
+      }
+      <Input
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value)
+          setAlert(false)
+        }}
+        placeholder='Input post title'
+        style={{
+          width: '100%',
+          marginTop: '20px',
+          border: '0.2px solid #ccc',
+          padding: '10px',
+          textDecoration: 'none',
+     }}></Input>
+      <Card sx={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: '20px',
+      }}>
+        <Toolbar
+                    editor={editor}
+                    defaultConfig={toolbarConfig}
+                    mode="default"
+                    style={{ borderBottom: '1px solid #ccc' }}
+                />
+                <Editor
+                    defaultConfig={editorConfig}
+                    value={html}
+                    onCreated={setEditor}
+          onChange={editor => {
+            setHtml(editor.getHtml())
+            setAlert(false)}}
+          mode="default"
+          style={{
+            width: '100%',
+            height: '300px',
+          }}
+                />
+      </Card>
+      <Button
+        onClick={() => {
+          setPosts((posts) => {
+            return [
+              ...posts,
+              {
+                title: title,
+                content: html,
+              }
+            ]
+          })
+          setAlert(true)
+          setTitle('')
+          setHtml('')
+        }}
+        sx={{
+        marginTop: '20px',
+        width: '40%',
+        backgroundColor: '#532BC5',
+        color: 'white',
+      }}>
+        <CheckIcon></CheckIcon>
+        Post</Button>
+    </div>
+    
+
+    )
+}
+
+export default MyEditor
